563eefe2526d8568c101e940936d3211ce332bac
Adam Fahmil <adam.ilmi@logswalker.com>
2022-06-05 02:01:39 +0700

    Learn Column, Row, and Stack for widget

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -8,21 +8,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // theme: new ThemeData(scaffoldBackgroundColor: Colors.green),
       home: Scaffold(
-        body: const Center(
-            child: Text(
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              backgroundColor: Colors.amber,
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Joan'),
-        )),
-        appBar: AppBar(title: const Text('My App')),
+        appBar: AppBar(title: Text('Column')),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                width: 200,
+                height: 50,
+                color: Colors.green,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.red,
+              ),
+              Container(
+                width: 100,
+                height: 30,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 150,
+                height: 90,
+                color: Colors.black,
+              )
+            ]),
       ),
     );
   }
