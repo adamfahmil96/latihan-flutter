860afa37a40bf261b3674bfe4b6bc0014976c508
Adam Fahmil <adam.ilmi@logswalker.com>
2022-06-05 21:07:02 +0700

    Basic ListView with ListView.builder

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -5,34 +5,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  List<Widget> myList = [
-    Container(
-      width: 300,
-      height: 100,
-      color: Colors.green,
-    ),
-    Container(
-      width: 300,
-      height: 100,
-      color: Colors.red,
-    ),
-    Container(
-      width: 300,
-      height: 100,
-      color: Colors.blue,
-    ),
-    Container(
-      width: 300,
-      height: 100,
-      color: Colors.black,
-    )
-  ];
+  List<Color> myColor = [Colors.red, Colors.green, Colors.blue, Colors.amber];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('List View')),
-        body: ListView(scrollDirection: Axis.horizontal, children: myList),
+        appBar: AppBar(title: Text('List View with Builder')),
+        body: ListView.builder(
+          itemCount: myColor.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 300,
+              width: 300,
+              color: myColor[index],
+            );
+          },
+        ),
       ),
     );
   }
