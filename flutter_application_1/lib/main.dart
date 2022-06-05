434f1b6e9f6e5cee2a511c3396a999fd92f80f54
Adam Fahmil <adam.ilmi@logswalker.com>
2022-06-05 21:01:10 +0700

    Basic ListView

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -5,36 +5,34 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  List<Widget> myList = [
+    Container(
+      width: 300,
+      height: 100,
+      color: Colors.green,
+    ),
+    Container(
+      width: 300,
+      height: 100,
+      color: Colors.red,
+    ),
+    Container(
+      width: 300,
+      height: 100,
+      color: Colors.blue,
+    ),
+    Container(
+      width: 300,
+      height: 100,
+      color: Colors.black,
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Column')),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Container(
-                width: 200,
-                height: 50,
-                color: Colors.green,
-              ),
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.red,
-              ),
-              Container(
-                width: 100,
-                height: 30,
-                color: Colors.blue,
-              ),
-              Container(
-                width: 150,
-                height: 90,
-                color: Colors.black,
-              )
-            ]),
+        appBar: AppBar(title: Text('List View')),
+        body: ListView(scrollDirection: Axis.horizontal, children: myList),
       ),
     );
   }
