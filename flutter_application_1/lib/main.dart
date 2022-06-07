bff647418290854715e4da626f5ec0c3eda74c7e
Adam Fahmil <adam.ilmi@logswalker.com>
2022-06-07 22:03:25 +0700

    Learn basic of List Tile

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -5,25 +5,41 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final List<Color> myColor = [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.amber
-  ];
-  final List<Widget> myList = List.generate(
-      100,
-      (index) => Text(
-            '${index + 1}',
-            style: TextStyle(fontSize: 20 + double.parse(index.toString())),
-          ));
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('List View with Builder')),
+        appBar: AppBar(title: const Text('List Tile')),
         body: ListView(
-          children: myList,
+          children: const [
+            ListTile(
+              contentPadding: EdgeInsets.all(20),
+              title: Text('Adam Fahmil'),
+              subtitle: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              leading: CircleAvatar(),
+              trailing: Text('10:00 PM'),
+              // tileColor: Colors.green,
+              // dense: true,
+            ),
+            Divider(color: Colors.black),
+            ListTile(
+              title: Text('Eva Nur'),
+              subtitle: Text('Halo selamat pagi...'),
+              leading: CircleAvatar(),
+              trailing: Text('09:00 AM'),
+            ),
+            Divider(color: Colors.black),
+            ListTile(
+              title: Text('Safira Nur'),
+              subtitle: Text('Mas tolong jemput aku dong, please...'),
+              leading: CircleAvatar(),
+              trailing: Text('01:05 PM'),
+            ),
+          ],
         ),
       ),
     );
