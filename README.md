f86adee35c8b2ab885a0699abe55a97047596b09
Adam Fahmil <adamfahmil020@gmail.com>
2022-06-11 06:51:22 +0700

    Basic of Extract Widget, case: ChatItem

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 This is my personal documentation of self-study on Flutter.
 
 ## Assets
-[Lorem Picsum](https://picsum.photos/)
+Generate random images: [Lorem Picsum](https://picsum.photos/)
+Generate random data (data dummy): [Faker](https://pub.dev/)
 
 ## References
 [Tutorial Flutter Indonesia NEW 2021](https://www.youtube.com/playlist?list=PL7jdfftn7HKsfTtv8FOaTbLIf7feiQTRu)
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,25 +6,67 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var faker = new Faker();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Image Widget')),
-        body: Center(
-            child: Container(
-          width: 350,
-          height: 500,
-          color: Colors.amber,
-          child: Image.asset('images/Untitled.png'),
-          // child: Image(
-          //   // image: AssetImage('images/Untitled.png') // Image from Asset
-          //   image: NetworkImage(
-          //       'https://picsum.photos/200/300'), // Image from Network
-          //   fit: BoxFit.cover,
-          // ),
-        )),
+        appBar: AppBar(title: Text('Extract Widget')),
+        // body: ListView( // Using Manual ListView
+        //   children: [
+        //     ChatItem(
+        //       imageUrl: 'https://picsum.photos/id/1005/200/300',
+        //       title: faker.person.name(),
+        //       subtitle: faker.lorem.sentence(),
+        //     ),
+        //     ChatItem(
+        //       imageUrl: 'https://picsum.photos/id/1003/200/300',
+        //       title: faker.person.name(),
+        //       subtitle: faker.lorem.sentence(),
+        //     ),
+        //     ChatItem(
+        //       imageUrl: 'https://picsum.photos/id/1015/200/300',
+        //       title: faker.person.name(),
+        //       subtitle: faker.lorem.sentence(),
+        //     ),
+        //     ChatItem(
+        //       imageUrl: 'https://picsum.photos/id/1025/200/300',
+        //       title: faker.person.name(),
+        //       subtitle: faker.lorem.sentence(),
+        //     ),
+        //   ],
+        // ),
+        body: ListView.builder(
+            // Geneate 30 random chats using ListView Builder
+            itemCount: 30,
+            itemBuilder: (context, index) {
+              return ChatItem(
+                  imageUrl: 'https://picsum.photos/id/$index/200/300',
+                  title: faker.person.name(),
+                  subtitle: faker.lorem.sentence());
+            }),
       ),
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+
+  const ChatItem(
+      {required this.imageUrl, required this.title, required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      trailing: Text('10:00 PM'),
     );
   }
 }
