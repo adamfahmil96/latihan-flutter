61a86e94edb63e87b184c77d11d911b624d64edf
Adam Fahmil <adamfahmil020@gmail.com>
2022-06-10 22:06:50 +0700

    Basic of Image Widget

--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 
 This is my personal documentation of self-study on Flutter.
 
+## Assets
+[Lorem Picsum](https://picsum.photos/)
+
 ## References
 [Tutorial Flutter Indonesia NEW 2021](https://www.youtube.com/playlist?list=PL7jdfftn7HKsfTtv8FOaTbLIf7feiQTRu)
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,38 +9,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('List Tile')),
-        body: ListView(
-          children: const [
-            ListTile(
-              contentPadding: EdgeInsets.all(20),
-              title: Text('Adam Fahmil'),
-              subtitle: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              leading: CircleAvatar(),
-              trailing: Text('10:00 PM'),
-              // tileColor: Colors.green,
-              // dense: true,
-            ),
-            Divider(color: Colors.black),
-            ListTile(
-              title: Text('Eva Nur'),
-              subtitle: Text('Halo selamat pagi...'),
-              leading: CircleAvatar(),
-              trailing: Text('09:00 AM'),
-            ),
-            Divider(color: Colors.black),
-            ListTile(
-              title: Text('Safira Nur'),
-              subtitle: Text('Mas tolong jemput aku dong, please...'),
-              leading: CircleAvatar(),
-              trailing: Text('01:05 PM'),
-            ),
-          ],
-        ),
+        appBar: AppBar(title: Text('Image Widget')),
+        body: Center(
+            child: Container(
+          width: 350,
+          height: 500,
+          color: Colors.amber,
+          child: Image.asset('images/Untitled.png'),
+          // child: Image(
+          //   // image: AssetImage('images/Untitled.png') // Image from Asset
+          //   image: NetworkImage(
+          //       'https://picsum.photos/200/300'), // Image from Network
+          //   fit: BoxFit.cover,
+          // ),
+        )),
       ),
     );
   }
