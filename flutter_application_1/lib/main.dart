835f86d77d6df6f2fb4e876a7cc435ef51b5ed13
Adam Fahmil <adamfahmil020@gmail.com>
2022-06-12 20:13:18 +0700

    Learn basic of Mapping List

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,101 +1,83 @@
-import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // Example for Stateful, the variable of 'counter' will change on the UI
-  int counter = 1;
-
+class MyApp extends StatelessWidget {
+  final List<Map<String, dynamic>> myList = [
+    {
+      "Name": "Adam",
+      "Age": 25,
+      "favColor": [
+        "Blue",
+        "Black",
+        "Amber",
+        "Green",
+        "White",
+        "Pink",
+        "Green light"
+      ]
+    },
+    {
+      "Name": "Eva",
+      "Age": 24,
+      "favColor": ["Red", "Black", "Green"]
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Dynamic Apps')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(counter.toString(),
-                style: TextStyle(
-                    fontSize: 50 +
-                        double.parse(counter
-                            .toString()))), // the font size will change every updated on counter
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    if (counter != 0) {
-                      setState(() {
-                        // re-render the UI, will build new context
-                        counter--;
-                      });
-                    }
-                    print(counter);
-                  },
-                  child: Icon(Icons.remove),
+          appBar: AppBar(title: Text('ID Apps')),
+          body: ListView(
+            children: myList.map((data) {
+              List myFavColor = data["favColor"];
+              return Card(
+                margin: EdgeInsets.all(30),
+                color: Colors.black.withOpacity(0),
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // avatar and ID data
+                      Row(
+                        children: [
+                          CircleAvatar(),
+                          SizedBox(width: 20),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Name: ${data["Name"]}'),
+                              Text('Age: ${data["Age"]}')
+                            ],
+                          )
+                        ],
+                      ),
+                      // favourite color
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: myFavColor.map((color) {
+                            return Container(
+                              color: Colors.amber,
+                              margin: EdgeInsets.symmetric(
+                                vertical: 15,
+                                horizontal: 8,
+                              ),
+                              padding: EdgeInsets.all(10),
+                              child: Text(color),
+                            );
+                          }).toList(),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      counter++;
-                    });
-                    print(counter);
-                  },
-                  child: Icon(Icons.add),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+              );
+            }).toList(),
+          )),
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   // Example for Stateless, the variable of 'counter' did not change on the UI
-//   int counter = 1;
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(title: Text('Dynamic Apps')),
-//         body: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           children: [
-//             Text(counter.toString(), style: TextStyle(fontSize: 20)),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               children: [
-//                 ElevatedButton(
-//                   onPressed: () {
-//                     if (counter != 0) {
-//                       counter--;
-//                     }
-//                     print(counter);
-//                   },
-//                   child: Icon(Icons.remove),
-//                 ),
-//                 ElevatedButton(
-//                   onPressed: () {
-//                     counter++;
-//                     print(counter);
-//                   },
-//                   child: Icon(Icons.add),
-//                 ),
-//               ],
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
